b6d8cd154b27ec7eb86a70cd574764f459b64588
devferx <ferkki@hotmail.com>
2021-04-17 21:36:31 -0400

    :art: [UPDATE] global styles file

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
 import React from "react";
-import { Global, css } from "@emotion/react";
+import { GlobalStyles } from "./globalStyles";
 import Home from "./pages/Home";
 
 const App = () => {
   return (
     <>
-      <Global
-        styles={css`
-          :root {
-            --primary-gradient: linear-gradient(
-              113.54deg,
-              #fb3a78 -39.39%,
-              #fc705e 74.54%
-            );
-            --black-1: #2c2c2c;
-            --grey: #5c5c5c;
-            --grey-2: #dfe3ea;
-            --bg: #f5f7fa;
-            --white: #ffffff;
-          }
-
-          * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-          }
-
-          body {
-            max-height: 100vh;
-            background-color: var(--bg);
-          }
-        `}
-      />
+      <GlobalStyles />
       <Home />
     </>
   );
